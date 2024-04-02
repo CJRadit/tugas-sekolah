ff9dfdfdd64bdef1992af03461e0d85886429610
CJRadit <cjradit@outlook.co.id>
2024-04-02 08:58:45 +0700

    c-terbesar: init

---
+++ c-terbesar/terbesar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char V[] = { 10, 4, 2, 5, 23, 8, 9, 2, 19, 5 };
+    int terbesar = V[0];
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (V[i] > terbesar) terbesar = V[i];
+    }
+    printf("Terbesar: %d", terbesar);
+
+    return 0;
+}
