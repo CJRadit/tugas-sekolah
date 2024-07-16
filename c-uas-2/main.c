9b7d949675c6787507d3511122c918bade129b87
CJRadit <44603345+CJRadit@users.noreply.github.com>
2024-07-16 11:05:36 +0700

    feat(c-uas-2): Update via web

--- c-uas-2/main.c
+++ c-uas-2/main.c
@@ -9,63 +9,93 @@ typedef struct List
     int desc[15];
     int min;
     int max;
-};
+} List;
 
-void input()
+void input(List *list)
 {
-    
-}
-
-int main()
-{
-    struct List list;
-
     for (int i = 0; i < 15; i++)
     {
         printf("Input angka ke-%d: ", i + 1);
-        scanf("%d", &angka[i]);
-        asc[i] = angka[i];
-        desc[i] = angka[i];
-    }
+        scanf("%d", &list->angka[i]);
+        list->asc[i] = list->angka[i];
+        list->desc[i] = list->angka[i];
+    }    
+}
 
+void sort(List *list)
+{
     for (int i = 0; i < 15 - 1; i++)
     {
         for (int j = 0; j < 15 - 1 - i; j++)
         {
             // Ascending
-            if (asc[j] > asc[j + 1])
+            if (list->asc[j] > list->asc[j + 1])
             {
-                int tmp = asc[j];
-                asc[j] = asc[j + 1];
-                asc[j + 1] = tmp;
+                int tmp = list->asc[j];
+                list->asc[j] = list->asc[j + 1];
+                list->asc[j + 1] = tmp;
             }
             // Descending
-            if (desc[j] < desc[j + 1])
+            if (list->desc[j] < list->desc[j + 1])
             {
-                int tmp = desc[j];
-                desc[j] = desc[j + 1];
-                desc[j + 1] = tmp;
+                int tmp = list->desc[j];
+                list->desc[j] = list->desc[j + 1];
+                list->desc[j + 1] = tmp;
             }
         }
     }
+}
 
-    printf("Asc:");
+void other_stuff(List *list)
+{
+    // int min = list->angka[0];
+    // int max = list->angka[0];
+    list->sum = 0;
     for (int i = 0; i < 15; i++)
     {
-        printf(" %d", asc[i]);
+        // if (list->angka[i] < min)
+        //     min = list->angka[i];
+        // if (list->angka[i] > max)
+        //     max = list->angka[i];
+        list->sum += list->angka[i];
     }
+    list->mean = (float)list->sum / 15.f;
+    list->min = list->asc[0];
+    list->max = list->desc[0];
+}
+
+int main()
+{
+    List *list, _list;
+    list = &_list;
+    input(list);
+    sort(list);
+    other_stuff(list);
+
+    // Print stuff
+    printf("\nAngka:");
+    for (int i = 0; i < 15; i++)
+    {
+        printf(" %d", list->angka[i]);
+    }
+
+    printf("\nSum: %d", list->sum);
+    
+    printf("\nMean: %2f", list->mean);
+    
+    printf("\nAsc:");
+    for (int i = 0; i < 15; i++)
+    {
+        printf(" %d", list->asc[i]);
+    }
+
     printf("\nDesc:");
     for (int i = 0; i < 15; i++)
     {
-        printf(" %d", desc[i]);
+        printf(" %d", list->desc[i]);
     }
-    int sum = 0;
-    for (int i = 0; i < 15; i++)
-    {
-        sum += angka[i];
-    }
-    float mean = (float) sum / 15.f;
-    printf("\nSum: %d", sum);
-    printf("\nMean: %f", mean);
+    
+    printf("\nMin: %d", list->min);
+    
+    printf("\nMax: %d", list->max);
 }
-
